611103b95006a6a0389a27e31276138f4958c991
finch <finchthomasward@gmail.com>
2025-10-13 17:28:17 +1300

    interaction added

--- config.py
+++ config.py
@@ -24,15 +24,17 @@ class Config:
     is_multi_gpu = False
 
     is_interacting = True
-    interactive_model = "v8.5"
-    interactive_checkpoint = "chkp1"
-    interactive_latent = "idx_35"
+    interactive_model = "v8.52"
+    interactive_checkpoint = "main"
+    interactive_latent = "idx_35.pt"
+    is_latent_init_noise = False
+    is_latent_init_zeros = False
     
 
     effective_batch_size = 256
 
     sample_every_x_batches = 4998 #avoid being divisible by latent_persistence_turns
-    inference_samples = 300
+    inference_samples = 100
     img_dir = 'images'
 
     model_resolution = (384, 512)
@@ -57,8 +59,11 @@ class Config:
     rotation_probability = 0.7
     
     initial_pages = [
-        r"https://www.google.com/maps/@-38.0633252,175.5396599,3a,75y,251.25h,90t/data=!3m7!1e1!3m5!1sDoszpl71ckFRT0ZDix5UHA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3DDoszpl71ckFRT0ZDix5UHA%26yaw%3D251.24602!7i13312!8i6656?entry=ttu&g_ep=EgoyMDI1MDkxMC4wIKXMDSoASAFQAw%3D%3D",  
-        r"https://www.google.com/maps/@-38.1054899,175.5949416,3a,75y,253.91h,91.66t/data=!3m7!1e1!3m5!1sJijCcQ6jydZMzlbjnK0_7A!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-1.6631163651967285%26panoid%3DJijCcQ6jydZMzlbjnK0_7A%26yaw%3D253.90960036050717!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDkxNy4wIKXMDSoASAFQAw%3D%3D",
+        r"https://www.google.com/maps/@-34.7826845,173.1006281,3a,75y,48.05h,109.63t/data=!3m7!1e1!3m5!1sjWIbsBBMN4YiWwdgLl5Y5A!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-19.63436488427803%26panoid%3DjWIbsBBMN4YiWwdgLl5Y5A%26yaw%3D48.045970941061924!7i13312!8i6656?entry=ttu&g_ep=EgoyMDI1MDkzMC4wIKXMDSoASAFQAw%3D%3D",
+        r"https://www.google.com/maps/@-37.1915928,174.654654,3a,75y,265.22h,85.04t/data=!3m7!1e1!3m5!1sxCmXgULR3Wewk4gGIGPsow!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D4.95869070845373%26panoid%3DxCmXgULR3Wewk4gGIGPsow%26yaw%3D265.2245735720425!7i13312!8i6656?entry=ttu&g_ep=EgoyMDI1MDkzMC4wIKXMDSoASAFQAw%3D%3D",
+        r"https://www.google.com/maps/@-38.2781874,178.2625244,3a,75y,342.95h,88.49t/data=!3m7!1e1!3m5!1sN_l8m4iP1o-8EI-4baOS2g!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D1.5134699273536683%26panoid%3DN_l8m4iP1o-8EI-4baOS2g%26yaw%3D342.9537239759422!7i13312!8i6656?entry=ttu&g_ep=EgoyMDI1MDkzMC4wIKXMDSoASAFQAw%3D%3D",
+        r"https://www.google.com/maps/@-35.0347671,173.9107666,3a,75y,287.18h,96.07t/data=!3m7!1e1!3m5!1s2ZeaVLCvjeBDiQCRYSYttg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-6.067976971432643%26panoid%3D2ZeaVLCvjeBDiQCRYSYttg%26yaw%3D287.18365123558544!7i13312!8i6656?entry=ttu&g_ep=EgoyMDI1MDkzMC4wIKXMDSoASAFQAw%3D%3D",
+        r"https://www.google.com/maps/@-34.4401226,172.7131244,3a,75y,247.7h,86.57t/data=!3m7!1e1!3m5!1san-k3n7Q5u_NfQSwzrEpHw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D3.430265383680293%26panoid%3Dan-k3n7Q5u_NfQSwzrEpHw%26yaw%3D247.69731166955927!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDkzMC4wIKXMDSoASAFQAw%3D%3D",
         ] * 3
 
 #     [ 
